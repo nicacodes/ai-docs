88936f82a641be0d607af63e5f32a8d005262549
Moises Barillas <moisesrocha717@gmail.com>
2026-01-08 08:53:51 -0600

    feat: ajustar anchos máximos en layout y post para mejorar la presentación

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
   adapter: node({
     mode: 'standalone',
   }),
@@ -33,7 +32,4 @@ export default defineConfig({
   },
 
   integrations: [react()],
-  server: {
-    host: '0.0.0.0',
-  },
 });
